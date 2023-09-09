fa5b0542bfe202d8fc4cf6611af0eedba9d006c5
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-09-10 00:57:19 +0530

    Update App.js

--- login-signup-app/src/App.js
+++ login-signup-app/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './display/Home';
 import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
+         <div>
         <Routes>
+          <Route exact path="/" element={<Home/>}/>
           <Route exact path="/login" element={<Login/>}/>
           <Route exact path="/signup" element={<Signup/>}/>
         </Routes>
+        </div>
       </Router>
     </>
     
